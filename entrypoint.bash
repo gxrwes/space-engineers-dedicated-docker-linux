02816b1efd2188a97116fc5a74b718b97c5d4238
wes <wesley.st96@yahoo.de>
2025-01-06 18:51:05 +0100

    issue

--- entrypoint.bash
+++ entrypoint.bash
@@ -1,37 +1,34 @@
 #!/bin/bash
 
-# #check if /appdata/space-engineers/config/World is a folder
+# Check if /appdata/space-engineers/config/World is a folder
 if [ ! -d "/appdata/space-engineers/World" ]; then
   echo "World folder does not exist, exiting"
   exit 129
 fi
 
-# #check if /appdata/space-engineers/config/World/Sandbox.sbc exists and is a file
+# Check if /appdata/space-engineers/config/World/Sandbox.sbc exists and is a file
 if [ ! -f "/appdata/space-engineers/World/Sandbox.sbc" ]; then
   echo "Sandbox.sbc file does not exist, exiting."
   exit 130
 fi
 
-# #check if /appdata/space-engineers/config/SpaceEngineers-Dedicated.cfg is a file
+# Check if /appdata/space-engineers/config/SpaceEngineers-Dedicated.cfg is a file
 if [ ! -f "/appdata/space-engineers/SpaceEngineersDedicated/SpaceEngineers-Dedicated.cfg" ]; then
   echo "SpaceEngineers-Dedicated.cfg file does not exist, exiting."
   exit 131
 fi
 
-#set <LoadWorld> to the correct value
-cat /appdata/space-engineers/SpaceEngineersDedicated/SpaceEngineers-Dedicated.cfg | sed -E '/.*LoadWorld.*/c\  <LoadWorld>Z:\\appdata\\space-engineers\\World</LoadWorld>' > /tmp/SpaceEngineers-Dedicated.cfg && cat /tmp/SpaceEngineers-Dedicated.cfg > /appdata/space-engineers/SpaceEngineersDedicated/SpaceEngineers-Dedicated.cfg
+# Set <LoadWorld> to the correct value
+sed -E '/.*LoadWorld.*/c\  <LoadWorld>Z:\\appdata\\space-engineers\\World</LoadWorld>' \
+  /appdata/space-engineers/SpaceEngineersDedicated/SpaceEngineers-Dedicated.cfg > /tmp/SpaceEngineers-Dedicated.cfg \
+  && mv /tmp/SpaceEngineers-Dedicated.cfg /appdata/space-engineers/SpaceEngineersDedicated/SpaceEngineers-Dedicated.cfg
 
-#set game port to the correct value
-#cat /appdata/space-engineers/SpaceEngineersDedicated/SpaceEngineers-Dedicated.cfg | sed -E '/.*ServerPort.*/c\  <ServerPort>27016</ServerPort>' > /tmp/SpaceEngineers-Dedicated.cfg && cat /tmp/SpaceEngineers-Dedicated.cfg > /appdata/space-engineers/SpaceEngineersDedicated/SpaceEngineers-Dedicated.cfg
-
-#configure plugins section in SpaceEngineers-Dedicated.cfg
-#get new plugins string
-
-if [ "$(ls -1 /appdata/space-engineers/Plugins/*.dll | wc -l)" -gt "0" ]; then
+# Configure plugins section in SpaceEngineers-Dedicated.cfg
+if [ "$(ls -1 /appdata/space-engineers/Plugins/*.dll 2>/dev/null | wc -l)" -gt "0" ]; then
   PLUGINS_STRING=$(ls -1 /appdata/space-engineers/Plugins/*.dll |\
-  awk '{ print "<string>" $0 "</string>" }' |\
-  tr -d '\n' |\
-  awk '{ print "<Plugins>" $0 "</Plugins>" }' )
+    awk '{ print "<string>" $0 "</string>" }' |\
+    tr -d '\n' |\
+    awk '{ print "<Plugins>" $0 "</Plugins>" }')
 else
   PLUGINS_STRING="<Plugins />"
 fi
@@ -39,13 +36,14 @@ fi
 SED_EXPRESSION_EMPTY="s/<Plugins \/>/${PLUGINS_STRING////\\/} /g"
 SED_EXPRESSION_FULL="s/<Plugins>.*<\/Plugins>/${PLUGINS_STRING////\\/} /g"
 
-#find and replace in SpaceEngineers-Dedicated.cfg for empty "<Plugins />" element
-cat /appdata/space-engineers/SpaceEngineersDedicated/SpaceEngineers-Dedicated.cfg | sed -E "$SED_EXPRESSION_EMPTY" > /tmp/SpaceEngineers-Dedicated.cfg && cat /tmp/SpaceEngineers-Dedicated.cfg > /appdata/space-engineers/SpaceEngineersDedicated/SpaceEngineers-Dedicated.cfg
+# Replace "<Plugins />" or "<Plugins>...</Plugins>" in the config file
+sed -E "$SED_EXPRESSION_EMPTY" /appdata/space-engineers/SpaceEngineersDedicated/SpaceEngineers-Dedicated.cfg > /tmp/SpaceEngineers-Dedicated.cfg \
+  && mv /tmp/SpaceEngineers-Dedicated.cfg /appdata/space-engineers/SpaceEngineersDedicated/SpaceEngineers-Dedicated.cfg
+sed -E "$SED_EXPRESSION_FULL" /appdata/space-engineers/SpaceEngineersDedicated/SpaceEngineers-Dedicated.cfg > /tmp/SpaceEngineers-Dedicated.cfg \
+  && mv /tmp/SpaceEngineers-Dedicated.cfg /appdata/space-engineers/SpaceEngineersDedicated/SpaceEngineers-Dedicated.cfg
 
-#find and replace in SpaceEngineers-Dedicated.cfg for filled out "<Plugins>...</Plugins>" element
-# sed can't handle multiple lines easily, so everything needs to be on a single line.
-cat /appdata/space-engineers/SpaceEngineersDedicated/SpaceEngineers-Dedicated.cfg | sed -E "$SED_EXPRESSION_FULL" > /tmp/SpaceEngineers-Dedicated.cfg && cat /tmp/SpaceEngineers-Dedicated.cfg > /appdata/space-engineers/SpaceEngineersDedicated/SpaceEngineers-Dedicated.cfg
+# Run SteamCMD and update the game
+runuser -l wine -c 'steamcmd +force_install_dir /appdata/space-engineers/SpaceEngineersDedicated +@sSteamCmdForcePlatformType windows +login anonymous +app_update 298740 validate +quit'
 
-
-runuser -l wine bash -c 'steamcmd +login anonymous +@sSteamCmdForcePlatformType windows +force_install_dir /appdata/space-engineers/SpaceEngineersDedicated +app_update 298740 +quit'
-runuser -l wine bash -c '/entrypoint-space_engineers.bash'
+# Start the Space Engineers Dedicated Server
+runuser -l wine -c '/entrypoint-space_engineers.bash'
